5d90b78919b3f558f62cfc668ca6843705c1885e
tlingf <tlingf@gmail.com>
2014-01-29 08:30:54 -0800

    add print, comments

--- lshtc.py
+++ lshtc.py
@@ -1,3 +1,7 @@
+# Very basic script to run initial results for Kaggle's LSHTC.
+# https://www.kaggle.com/c/lshtc
+# Code still in early testing mode
+
 # SETTINGS
 ROOT_DIR = '/Users/ling/lshtc/'
 
@@ -11,20 +15,24 @@ from sklearn.neighbors import KNeighborsClassifier
 # (head -1000 train.csv ) > train_1000.csv
 # delete first line (Data)
 # Change all comma space to comma :%s/, /,/g
-
+print "Loading data..."
 data = load_svmlight_file(ROOT_DIR + "train_1000.csv", multilabel = True)
 
 # Turn labels into binary columns
+print "Binarizing labels..."
 lb = preprocessing.LabelBinarizer()
 lb.fit(data[1])
-x = lb.fit_transform(data[1])
+y_train = lb.fit_transform(data[1])
 
 # Multi-label classifier learning
+print "Fitting classifier..."
 classif = OneVsRestClassifier(KNeighborsClassifier())
-classif.fit(data[0], x)
+classif.fit(data[0], y_train)
 
 # Testing if plain works for now - predict on training set
-prediction = classif.predict(x)
+print "Classifying..."
+prediction = classif.predict(data[0])
 
 # Scoring - need to finish
+print "Scoring..."
 data[0].toarray()
